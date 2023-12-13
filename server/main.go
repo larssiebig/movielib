21fcdde05558c08d0a56efef6c73d0468ed0625b
larssiebig <siebig.lars.ls@gmail.com>
2023-12-13 02:10:25 +0100

    added first api route to post movies into slice

--- server/main.go
+++ server/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Movie struct {
+	ID      int    `json:"id"`
 	Title   string `json:"title"`
 	Genre   string `json:"genre"`
 	Year    int    `json:"year"`
@@ -15,12 +15,26 @@ type Movie struct {
 }
 
 func main() {
-	fmt.Println("Hello World")
-
 	app := fiber.New()
+
+	movies := []Movie{}
 
 	app.Get("/moviestore", func(c *fiber.Ctx) error {
 		return c.SendString("Okay")
+	})
+
+	app.Post("/api/movies", func(c *fiber.Ctx) error {
+		movie := &Movie{}
+
+		if err := c.BodyParser(movie); err != nil {
+			return err
+		}
+
+		movie.ID = len(movies) + 1
+
+		movies = append(movies, *movie)
+
+		return c.JSON(movies)
 	})
 
 	log.Fatal(app.Listen(":4000"))
