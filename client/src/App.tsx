d16cf26bc51321460eff7f3606cff05690f2d837
larssiebig <lars.siebig.ls@gmail.com>
2023-12-14 02:44:09 +0100

    added display of existing movies and proper typing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,41 @@
-import { MantineProvider,  Box } from '@mantine/core'
+import { MantineProvider,  Box, Flex } from '@mantine/core'
 import useSWR from 'swr'
 import "./App.css"
 import AddMovie from './components/AddMovie'
+
+export interface Movie {
+  id: string
+  title: string
+  genre: string
+  year: string
+  favourite: boolean
+}
 
 export const ENDPOINT = 'http://localhost:8080'
 
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((res) => res.json())
 
+
 function App() {
-  
-  const { data, mutate } = useSWR('api/movies', fetcher)
+  const { data, mutate } = useSWR<Movie[]>('api/movies', fetcher)
 
   return (
     <MantineProvider>
-        <Box>
-          {JSON.stringify(data)}
-          <AddMovie />
-        </Box>
+      <Box>
+        <Flex direction="row" gap={100} align="center" justify="center" className="App">
+          {data?.map((movie) => (
+            <div key={movie.id}>
+              <div>
+                <h2>{movie.title}</h2>
+                <p>{movie.genre}</p>
+                <p>{movie.year}</p>
+                <p>{movie.favourite}</p>
+              </div>
+            </div>
+          ))}
+        </Flex>
+        <AddMovie mutate={mutate} />
+      </Box>
     </MantineProvider>
     )
 }
